53bad900f3fc5c115856f624aaae8f1d9f2bcf71
PatitPawan <pp1197barik@gmail.com>
2019-06-20 03:02:11 +0530

    Add files via upload

--- Bookstore/src/main/java/com/cg/bookstore/controllers/BookstoreServicesController.java
+++ Bookstore/src/main/java/com/cg/bookstore/controllers/BookstoreServicesController.java
@@ -1,18 +1,22 @@
 package com.cg.bookstore.controllers;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.cg.bookstore.beans.Address;
+import com.cg.bookstore.beans.Admin;
 import com.cg.bookstore.beans.Book;
 import com.cg.bookstore.beans.Customer;
 import com.cg.bookstore.beans.OrderItem;
@@ -29,6 +33,21 @@ public class BookstoreServicesController {
 	@RequestMapping(value= {"/sayHello"}, method=RequestMethod.GET)
 	public ResponseEntity<String> sayHello(){
 		return new ResponseEntity<String>("Hello World To all From RESTImpl",HttpStatus.OK);
+	}
+	@RequestMapping(value= {"/acceptCustomerDetails"}, method=RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+	public ResponseEntity<String> acceptCustomerDetails(@ModelAttribute Customer customer){
+		customer = bookstoreServices.acceptCustomerDetails(customer);
+		return new ResponseEntity<>("Customer Details successfully added. \n Customer Id : "+customer.getCustomerEmailId(),HttpStatus.OK);
+	}
+	@RequestMapping(value= {"/acceptBookDetails"}, method=RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+	public ResponseEntity<String> acceptBookDetails(@ModelAttribute Book book){
+		book = bookstoreServices.acceptBookDetails(book);
+		return new ResponseEntity<>("Customer Details successfully added. \n Book Id : "+book.getBookIsbn(),HttpStatus.OK);
+	}
+	@RequestMapping(value= {"/acceptAdminDetails"}, method=RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+	public ResponseEntity<String> acceptAdminDetails(@ModelAttribute Admin admin){
+		admin = bookstoreServices.acceptAdminDetails(admin);
+		return new ResponseEntity<>("Customer Details successfully added. \n Book Id : "+admin.getAdminEmailId(),HttpStatus.OK);
 	}
 	@RequestMapping(value= {"/getBookDetails"}, method=RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE, headers = "Accept = application/json")
 	public ResponseEntity<Book> getBookDetailsRequestParam(@RequestParam long bookIsbn) throws BookDetailsNotFoundException{
@@ -62,8 +81,8 @@ public class BookstoreServicesController {
 		return new ModelAndView("home", "customer", bookstoreServices.customerLogin(customerEmailId, password));
 	}
 	@RequestMapping("/editProfile")
-	public ModelAndView resetPasswordAction(@RequestParam String customerEmailId, String password) throws CustomerDetailsNotFoundException{
-		return new ModelAndView("EditProfile", "customer", bookstoreServices.editProfile(customerEmailId, password));
+	public ModelAndView resetPasswordAction(@RequestParam String customerEmailId, Address customerAddress) throws CustomerDetailsNotFoundException{
+		return new ModelAndView("EditProfile", "customer", bookstoreServices.editProfile(customerEmailId, customerAddress ));
 	}
 	@RequestMapping("/removeBook")
 	public ModelAndView removeBookDetails(@RequestParam long bookIsbn, HttpSession session) throws BookDetailsNotFoundException {
